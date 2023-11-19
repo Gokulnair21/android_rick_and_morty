72669c77255d6bd06cf9b85824a1ce1495644365
Gokul Nair <gokul.nairmurali@gmail.com>
2023-11-19 22:20:41 +0530

    Updated Dependency for Preview

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,6 @@ plugins {
     id("com.apollographql.apollo3").version("3.7.3")
     id("com.google.dagger.hilt.android")
     kotlin("kapt")
-
-
 }
 
 
@@ -60,6 +58,14 @@ android {
     composeOptions {
         kotlinCompilerExtensionVersion = compose_version
     }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.2"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
 
 }
 
@@ -75,11 +81,26 @@ dependencies {
     implementation("androidx.fragment:fragment-ktx:1.6.1")
 
     //Compose
-    implementation("androidx.compose.ui:ui:$compose_version")
-    implementation("androidx.compose.material:material:$compose_version")
-    implementation("androidx.compose.ui:ui-tooling-preview:$compose_version")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:$compose_version")
-    androidTestImplementation( "androidx.compose.ui:ui-tooling:$compose_version")
+//    implementation(platform("androidx.compose:compose-bom:2023.10.01"))
+//    implementation("androidx.compose.material:material")
+//    implementation("androidx.compose.ui:ui-tooling-preview")
+//    implementation("androidx.compose.runtime:runtime")
+//    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+//    androidTestImplementation( "androidx.compose.ui:ui-tooling")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
 
     //Navigation Component
     implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
